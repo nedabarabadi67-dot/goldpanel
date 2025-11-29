7a4be6fc14bc6f794a309bd38a2cfbcd0bdff422
nedabarabadi67-dot <nedabarabadi.67@gmail.com>
2025-11-29 18:32:58 +0330

    شرح تغییر

--- gold_project/settings.py
+++ gold_project/settings.py
@@ -139,7 +139,7 @@ MEDIA_ROOT=BASE_DIR/'media'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+#STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -174,7 +174,7 @@ SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 
 import os
 
-#STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 
 #STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
